3f6d32be71c45d741e36b7570a93e9cc15902e04
Cole H <nightbat13131@gmail.com>
2026-04-19 11:39:05 -0700

    Duplicate messhes to keep UpgradeVisualizer3D results unique to the buildings

--- entities/player/tower_foundation/upgrade_visualizer.gd
+++ entities/player/tower_foundation/upgrade_visualizer.gd
@@ -6,6 +6,8 @@ func _ready() -> void:
 	_on_upgrade_changed()
 
 func set_upgrade_info(info: FoundationUpgrades) -> void: 
+	if _upgrades != null:
+		_upgrades.changed.disconnect(_on_upgrade_changed)
 	_upgrades = info
 	_upgrades.changed.connect(_on_upgrade_changed)
 	_on_upgrade_changed() ## apply starting upgrades
--- entities/player/tower_foundation/upgrade_visualizer_3g.gd
+++ entities/player/tower_foundation/upgrade_visualizer_3g.gd
@@ -3,9 +3,10 @@ class_name UpgradeVisualizer3D extends UpgradeVisualizer
 @export var nodes : Dictionary[FoundationUpgrades.UpgradeTypes, MeshInstance3D]
 
 func _ready() -> void:
-	for mesh in nodes.values():
-		mesh.get_mesh().set_material(StandardMaterial3D.new())
-		mesh.get_mesh().get_material().set_transparency(true)
+	for node in nodes.values():
+		node.set_mesh(node.get_mesh().duplicate())
+		node.get_mesh().set_material(StandardMaterial3D.new())
+		node.get_mesh().get_material().set_transparency(true)
 	super._ready()
 
 func _on_upgrade_changed() -> void:
